44edea9d2ab69ddc85bccc270d8e3179e66fc20d
Moorthi Kanthasamy <>
2023-04-29 17:20:25 +0530

    feat(googlelogin): next-auth added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,19 @@
-
+import { useSession, signIn, signOut } from "next-auth/react"
+import Link from "next/link"
 export default function Home() {
-  return <div>Home Component</div>
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
